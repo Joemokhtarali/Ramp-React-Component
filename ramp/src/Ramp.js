7f4df80a916ec84b66253528d55de3ff7a9dd4e8
joemokhtarali@gmail.com <joemokhtarali@gmail.com>
2020-12-03 12:14:10 -0500

    case undefined or falsy added render the Time component

--- ramp/src/Ramp.js
+++ ramp/src/Ramp.js
@@ -5,17 +5,33 @@ export default function Ramp(props) {
   let input = props.input;
 
   const renderApp = () => {
-    if (input === "") {
-      return (
-        <div style={{ textAlign: "center" }}>
-          <h3>Instructions</h3>
-          <ul>
-            <li>If the prop is undefined or falsy, the output should be the Time.</li>
-            <li>If the prop is an array, the output should be a list of divs.</li>
-            <li>If the prop is anything else, the output should be the same value.</li>
-          </ul>
-        </div>
-      );
+    switch (input) {
+      case "":
+        return (
+          <div style={{ textAlign: "center" }}>
+            <h3>Instructions</h3>
+            <ul>
+              <li>
+                If the prop is undefined or falsy, the output should be the
+                Time.
+              </li>
+              <li>
+                If the prop is an array, the output should be a list of divs.
+              </li>
+              <li>
+                If the prop is anything else, the output should be the same
+                value.
+              </li>
+            </ul>
+          </div>
+        );
+      case undefined || false || "undefined":
+        return (
+          <div> <Time /></div>
+        );
+
+      default:
+        return <div>{input}</div>;
     }
   };
 
